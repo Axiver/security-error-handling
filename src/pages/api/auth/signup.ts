3f1770fa45933a3e60557b82a2c1f929b43b8d1e
Xavier Teo <xavierteo26@gmail.com>
2023-03-24 20:53:08 +0800

    refactor: :sparkles: use next-connect in api routes

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,57 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 import PrismaClient from "../../../utils/prisma";
 import bcrypt from "bcryptjs";
+import apiHandler from "@/utils/apiHandler";
 
-type Response = {
-  success: boolean;
-  message?: string;
-};
+export default apiHandler.post(async (req, res) => {
+  const { name, email, password } = req.body;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
-  if (req.method === "POST") {
-    const { name, email, password } = req.body;
-
-    if (!email || !email.includes("@") || !password || password.trim().length < 8) {
-      res.status(422).json({ success: false, message: "Invalid input" });
-      return;
-    }
-
-    // Check if user already exists
-    const existingUser = await PrismaClient.users.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (existingUser) {
-      res.status(422).json({ success: false, message: "User already exists" });
-      return;
-    }
-
-    // Generate salt with 10 rounds
-    const salt = await bcrypt.genSalt(10);
-
-    // Hash password with bcrypt
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    let response = await PrismaClient.users.create({
-      data: {
-        name: name,
-        email: email,
-        image: null,
-        password: hashedPassword,
-        permissions: 0,
-        companyid: 1,
-      },
-    });
-
-    if (response.id) {
-      res.status(201).json({ success: true, message: "User created" });
-    } else {
-      res.status(500).json({ success: false, message: "Something went wrong" });
-    }
-  } else {
-    res.status(405).end();
+  if (!email || !email.includes("@") || !password || password.trim().length < 8) {
+    res.status(422).json({ success: false, message: "Invalid input" });
+    return;
   }
-}
+
+  // Check if user already exists
+  const existingUser = await PrismaClient.users.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (existingUser) {
+    res.status(422).json({ success: false, message: "User already exists" });
+    return;
+  }
+
+  // Generate salt with 10 rounds
+  const salt = await bcrypt.genSalt(10);
+
+  // Hash password with bcrypt
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  let response = await PrismaClient.users.create({
+    data: {
+      name: name,
+      email: email,
+      image: null,
+      password: hashedPassword,
+      permissions: 0,
+      companyid: 1,
+    },
+  });
+
+  if (response.id) {
+    res.status(201).json({ success: true, message: "User created" });
+  } else {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+});
--- src/pages/api/auth/testSignIn.ts
+++ src/pages/api/auth/testSignIn.ts
@@ -1,35 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import PrismaClient from "@prisma/client";
 import bcrypt from "bcryptjs";
+import apiHandler from "@/utils/apiHandler";
 
-type Response = {
-  success: boolean;
-  message?: string;
-};
+export default apiHandler.post(async (req, res) => {
+  const prisma = new PrismaClient.PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
-  if (req.method === "POST") {
-    const prisma = new PrismaClient.PrismaClient();
+  const user = await prisma.users.findUnique({
+    where: {
+      email: "karandeepsingh00@icloud.com",
+    },
+  });
 
-    const user = await prisma.users.findUnique({
-      where: {
-        email: "karandeepsingh00@icloud.com",
-      },
-    });
+  if (user && user.password) {
+    // Check  if the password is correct
+    const isPasswordCorrect = await bcrypt.compare("testtesttest", user.password);
 
-    if (user && user.password) {
-      // Check  if the password is correct
-      const isPasswordCorrect = await bcrypt.compare("testtesttest", user.password);
-
-      if (isPasswordCorrect) {
-        res.status(200).json({ success: true, message: "Logged in" });
-      } else {
-        res.status(401).json({ success: false, message: "Invalid credentials" });
-      }
+    if (isPasswordCorrect) {
+      res.status(200).json({ success: true, message: "Logged in" });
     } else {
       res.status(401).json({ success: false, message: "Invalid credentials" });
     }
   } else {
-    res.status(405).end();
+    res.status(401).json({ success: false, message: "Invalid credentials" });
   }
-}
+});
