295c72c6a707d232d3091638880dc8db7d06d559
Xavier Teo <xavierteo26@gmail.com>
2023-03-28 19:36:13 +0800

    fix: :bug: bug where access tokens could not be refreshed
    - when refreshing, we would try to update the 'revoked' column in the 'access_keys' table, but that would violate the foreign key constraint

--- src/pages/api/test-api.ts
+++ src/pages/api/test-api.ts
@@ -1,8 +1,6 @@
 import { apiHandler } from "@/utils/api";
 
-export default apiHandler({
-  allowAdminsOnly: true,
-}).get(async (req, res) => {
+export default apiHandler().get(async (req, res) => {
   console.log({ token: req.token });
 
   res.status(200).json({ success: true, message: "test complete" });
